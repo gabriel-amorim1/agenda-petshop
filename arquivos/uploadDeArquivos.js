f8b0f4968a1f33a7e720c4a12747b8cefa01e1b6
gabriel-amorim1 <therealamorim@gmail.com>
2021-06-09 14:19:15 -0300

    first commit

---
+++ arquivos/uploadDeArquivos.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+    const tiposValidos = ['jpg', 'png', 'jpeg'];
+
+    const tipo = path.extname(caminho);
+
+    const tipoEhValido = tiposValidos.indexOf(tipo.substring(1));
+
+    if (tipoEhValido === -1) {
+        console.log('Erro! Tipo inválido');
+    } else {
+        const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`;
+    
+        fs.createReadStream(caminho)
+            .pipe(fs.createWriteStream(novoCaminho))
+            .on('finish', () => callbackImagemCriada(novoCaminho));
+    }
+}
